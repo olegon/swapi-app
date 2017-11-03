06601c0890aac0dbb9f4699ec49c40ad47a8b3da
olegon <olegon.main@gmail.com>
2017-11-02 22:14:07 -0200

    Validação do ambiente
    - Disparo de exceção caso a variável de ambiente PORT não seja
    especificada.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const app = require('./app');
 
+const { PORT } = process.env;
+
+if (PORT == null) throw new Error('A variável de ambiente PORT não foi especificada.');
+
 app.listen(
     process.env.PORT,
     () => console.log(`Listeneting at http://localhost:${process.env.PORT}/`)
